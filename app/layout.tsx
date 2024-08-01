c9fc0f914bec2501e4d7ef231cd62f07a158dad6
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-01 13:34:09 +0600

    page title updated in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "@/assets/css/globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import '@/assets/css/globals.css';
 
 const geistSans = localFont({
-  src: "../assets/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+  src: '../assets/fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
 });
 const geistMono = localFont({
-  src: "../assets/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+  src: '../assets/fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
 });
 
 export const metadata: Metadata = {
-  title: "Connectify",
-  description: "Social media app",
+  title: {
+    template: '%s | Connectify',
+    default: 'Connectify',
+  },
+  description: 'Social media app',
 };
 
 const RootLayout = ({
@@ -22,12 +25,15 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning={true}>
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}`}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
   );
-}
+};
 
 export default RootLayout;
