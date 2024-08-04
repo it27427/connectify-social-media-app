97cdd9e0156b6a46cf10ff080f009f03d6f7f38f
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-04 10:18:57 +0600

    lucia-auth

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import prisma from './lib/prisma';
+import { Lucia } from 'lucia';
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV == ,
+    },
+  },
+});
