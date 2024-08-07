a73b8ef1c77d915401e5e0fcb46959dfc141775f
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-07 12:51:43 +0600

    signUpSchema validation done

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,4 +4,9 @@ const requiredString = z.string().trim().min(1, 'This field is required.');
 
 export const signUpSchema = z.object({
   email: requiredString.email('Invalid email address'),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    'Only letters, numbers, - and _ allowed'
+  ),
+  password: requiredString.min(8, 'Password must be at least 8 characters'),
 });
