1acf1f43b561e4c345b804367051555d6136540b
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-07 12:44:44 +0600

    requiredString

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,3 +1,7 @@
 import { z } from 'zod';
 
-export const signUpSchema = () => {};
+const requiredString = z.string().trim().min(1, 'This field is required.');
+
+export const signUpSchema = z.object({
+  email: requiredString.email('Invalid email address'),
+});
