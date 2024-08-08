726a444ac2b6c5514c343ec1db10ecb9ec6690ec
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-08 14:56:54 +0600

    passwordHash

--- app/(auth)/sign-up/actions.ts
+++ app/(auth)/sign-up/actions.ts
@@ -1,7 +1,18 @@
 'use server';
 
-import { SignUpValues } from '@/lib/validation';
+import { signUpSchema, SignUpValues } from '@/lib/validation';
+import { hash } from '@node-rs/argon2';
 
 export async function signUp(
   credentials: SignUpValues
-): Promise<{ error: string }> {}
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+    const passwordHash = await hash();
+  } catch (error) {
+    console.error(error);
+    return {
+      error: 'Something went wrong! Please try again.',
+    };
+  }
+}
