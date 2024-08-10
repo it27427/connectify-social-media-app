4aa3cb4f90ab3a2fd23b7de19f44c2b3c1d88590
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-10 11:27:23 +0600

    username, password parse from signinschema

--- app/(auth)/sign-in/actions.ts
+++ app/(auth)/sign-in/actions.ts
@@ -1,16 +1,18 @@
 'user server';
 
-import { SignInValues } from '@/lib/validation';
+import { signInSchema, SignInValues } from '@/lib/validation';
 import { isRedirectError } from 'next/dist/client/components/redirect';
 
 export async function signIn(
   credentials: SignInValues
 ): Promise<{ error: string }> {
   try {
+    const { username, password } = signInSchema.parse(credentials);
   } catch (error) {
-    if (isRedirectError) {
-    }
-
+    if (isRedirectError(error)) throw error;
     console.error(error);
+    return {
+      error: 'Something went wrong! Please try again.',
+    };
   }
 }
