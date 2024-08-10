5c69249526139e8a64965d36735575d49266bdf8
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-10 11:35:43 +0600

    validPassword configuration done

--- app/(auth)/sign-in/actions.ts
+++ app/(auth)/sign-in/actions.ts
@@ -25,7 +25,12 @@ export async function signIn(
       };
     }
 
-    const validPassword = await verify(existingUser.passwordHash, password);
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
   } catch (error) {
     if (isRedirectError(error)) throw error;
     console.error(error);
