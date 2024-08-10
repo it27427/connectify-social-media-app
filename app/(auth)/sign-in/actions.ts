4489aee5dc90db50760d97fe3a3778bcedb3768a
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-10 11:24:34 +0600

    isRedirectError

--- app/(auth)/sign-in/actions.ts
+++ app/(auth)/sign-in/actions.ts
@@ -1,5 +1,16 @@
 'user server';
 
 import { SignInValues } from '@/lib/validation';
+import { isRedirectError } from 'next/dist/client/components/redirect';
 
-export async function signIn(credentials: SignInValues) {}
+export async function signIn(
+  credentials: SignInValues
+): Promise<{ error: string }> {
+  try {
+  } catch (error) {
+    if (isRedirectError) {
+    }
+
+    console.error(error);
+  }
+}
