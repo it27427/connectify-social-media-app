e68fd31c4d6ce98be9931e347a026cd2b3bf445a
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-11 11:53:46 +0600

    SignUp-Thumbnail Added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next';
+import signUpThumbnail from '@/assets/images/signup.jpg';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Sign Up',
@@ -7,7 +9,20 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <main className='flex items-center justify-center h-screen'>
-      <h2>Sign Up Page</h2>
+      <div className='flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl shadow-2xl overflow-hidden bg-card'>
+        <div className='md:w-1/2'>
+          <h2>Sign Up Form</h2>
+        </div>
+
+        <Image
+          src={signUpThumbnail}
+          alt='SignUp-Thumbnail'
+          width={1000}
+          height={1000}
+          priority
+          className='w-1/2 hidden md:block'
+        />
+      </div>
     </main>
   );
 }
