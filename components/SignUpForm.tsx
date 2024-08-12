591af731703efc11025839470b7b69b329116f97
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-12 12:17:49 +0600

    useForm

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,7 +1,10 @@
 'use client';
 import { SignUpValues } from '@/lib/validation';
+import { useForm } from 'react-hook-form';
 
 const SignUpForm = () => {
+  const form = useForm<SignUpValues>();
+
   return (
     <div>
       <h2>SignUpForm</h2>
