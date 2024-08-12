dd8bca083d1383ec1dd797e38d672cd2e421fa39
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-12 12:40:10 +0600

    Username FormItem Created

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -2,7 +2,15 @@
 import { signUpSchema, SignUpValues } from '@/lib/validation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form } from './ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
 
 const SignUpForm = () => {
   const form = useForm<SignUpValues>({
@@ -18,7 +26,21 @@ const SignUpForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'></form>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='Username' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
     </Form>
   );
 };
