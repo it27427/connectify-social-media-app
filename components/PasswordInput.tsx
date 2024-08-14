cb6130a5756c66d7c48b630da074f443eabb7f17
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-14 11:57:40 +0600

    Input Type

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+import { Input } from './ui/input';
 
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-      <div>
-        <h2>PasswordInput</h2>
+      <div className='relative'>
+        <Input type={showPassword ? 'text' : 'password'} />
       </div>
     );
   }
