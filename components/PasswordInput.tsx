cd022682c846ffbcdc1192b4bd72da1cbeb98cb9
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-16 13:39:06 +0600

    InputProps Imported

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input } from './ui/input';
+import { Input, InputProps } from './ui/input';
 import { cn } from '@/lib/utils';
 import { Eye, EyeOff } from 'lucide-react';
 
