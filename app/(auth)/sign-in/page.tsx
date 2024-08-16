f39ed01e78d65f79d5fd4261e8b5408cbe04ba2d
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-16 13:53:35 +0600

    title-centered

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
 import signInThumbnail from '@/assets/images/signin.jpg';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -11,7 +12,20 @@ export default function page() {
     <main className='flex items-center justify-center h-screen'>
       <div className='flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl shadow-2xl overflow-hidden bg-card'>
         <div className='md:w-1/2 w-full space-y-10 overflow-y-auto p-10'>
-          <h1 className='text-3xl font-bold'>Sign in to Connectify</h1>
+          <h1 className='text-3xl font-bold text-center'>
+            Sign in to Connectify
+          </h1>
+
+          <div className='space-y-5'>
+            {/* <SignInForm /> */}
+
+            <p className='text-center'>
+              Don't have any account?{' '}
+              <Link href='/sign-up' className='hover:underline'>
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
 
         <Image
