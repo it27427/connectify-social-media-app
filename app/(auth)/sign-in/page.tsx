b20e7245280f95de3eedbda3052684dcb3a7da8b
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-16 13:51:09 +0600

    signInThumbnail added in sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
+import signInThumbnail from '@/assets/images/signin.jpg';
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -7,7 +9,20 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <main className='flex items-center justify-center h-screen'>
-      <h2>Sign In Page</h2>
+      <div className='flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl shadow-2xl overflow-hidden bg-card'>
+        <div className='md:w-1/2 w-full space-y-10 overflow-y-auto p-10'>
+          <h1 className='text-3xl font-bold'>Sign in to Connectify</h1>
+        </div>
+
+        <Image
+          src={signInThumbnail}
+          alt='SignIn-Thumbnail'
+          width={1000}
+          height={1000}
+          priority
+          className='w-1/2 hidden md:block object-cover'
+        />
+      </div>
     </main>
   );
 }
