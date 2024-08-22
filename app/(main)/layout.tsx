11ddc1e64bb46769b2c4b22d4736f8c248206b5d
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-22 14:53:38 +0600

    Header import in layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,19 @@
 import { validateRequest } from '@/auth';
 import { redirect } from 'next/navigation';
 import SessionProvider from './SessionProvider';
+import Header from '@/components/Header';
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
 
   if (!session.user) redirect('/sign-in');
 
-  return <SessionProvider value={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider value={session}>
+      <Header />
+      <main>{children}</main>
+    </SessionProvider>
+  );
 };
 
 export default MainLayout;
