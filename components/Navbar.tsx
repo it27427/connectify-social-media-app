345b51aa699b8047676d0a3bc86521a7589b0d5f
it27427 <istiak.tushar.qtec@gmail.com>
2024-08-23 10:56:07 +0600

    Navbar homelink added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Navbar</h2>
+    <nav className='max-w-7xl mx-auto flex items-center justify-center flex-wrap px-5 py-3'>
+      <Link href={'/'} className='text-primary text-2xl font-bold'>
+        Connectify
+      </Link>
     </nav>
   );
 };
